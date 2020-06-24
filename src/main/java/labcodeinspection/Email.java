0dcc2f13c3e7c4301cb715824c061306a467d0ef
ginpajac <ginpajac@espol.edu.ec>
2020-06-24 15:24:30 -0400

    Bugs fixed

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -1,25 +1,104 @@
 package labcodeinspection;
 
+import java.util.Locale;
+
+/**
+ * 
+ * @author Ginger Jacome
+ *
+ */
 public class Email {
-
-	private String m_firstName;
-	private String m_lastName;
+	/** Members first name*/
+	private String firstName;
+	/** Members last name*/
+	private String lastName;
+	/** Members password*/
 	private String password = null;
+	/** Members deparment*/
 	private String department;
-	private int defaultpasswordLength = 8;
+	/** Default password length*/
+	private int dfpasswordLength = 8;
+	/** Members email*/
 	private String email;
-
-	public Email(String firstName, String lastName) {
-		this.m_firstName = firstName;
-		this.m_lastName = lastName;
+	
+	public String getFirstName() {
+		return firstName;
 	}
 
+	public void setFirstName(final String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(final String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(final String department) {
+		this.department = department;
+	}
+
+	/**
+	 * This method return the default password lenght
+	 * @return 
+	 */
+	public int getDfpasswordLength() {
+		return dfpasswordLength;
+	}
+	
+	/**
+	 * 
+	 * @param dfpasswordLength
+	 */
+	public void setDfpasswordLength(final int dfpasswordLength) {
+		this.dfpasswordLength = dfpasswordLength;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	
+	/**
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 */
+	public Email(final String firstName, final String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	/**
+	 * This method shows the member´s information
+	 */
 	public void showInfo() {
-		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
+		System.out.println("\nFIRST NAME= " + firstName + "\nLAST NAME= " + lastName);
 		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
 	}
-
-	public void setDeparment(int depChoice) {
+	/**
+	 * 
+	 * @param depChoice
+	 */
+	public void setDeparment(final int depChoice) {
 		switch (depChoice) {
 		case 1:
 			this.department = "sales";
@@ -33,19 +112,22 @@ public class Email {
 		}
 	}
 
-	private String randomPassword(int length) {
-		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
+	private String randomPassword(final int length) {
+		final String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
 		char[] password = new char[length];
 		for (int i = 0; i < length; i++) {
-			int rand = (int) (Math.random() * set.length());
+			final int rand = (int) (Math.random() * set.length());
 			password[i] = set.charAt(rand);
 		}
 		return new String(password);
 	}
 
+	/**
+	 * This method generates an email
+	 */
 	public void generateEmail() {
-		this.password = this.randomPassword(this.defaultpasswordLength);
-		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department
+		this.password = this.randomPassword(this.dfpasswordLength);
+		this.email = this.firstName.toLowerCase(Locale.getDefault()) + this.lastName.toLowerCase(Locale.getDefault())+ "@" + this.department
 				+ ".espol.edu.ec";
 	}
 }
